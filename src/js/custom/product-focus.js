68ed92e4ddfdb07d911b2e354bc743519ddb284b
Ricardo Lopes <ricardolopesroj@gmail.com>
2022-07-02 17:47:11 -0300

    pagina faltando somente responsividade

---
+++ src/js/custom/product-focus.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
+ let teste = document.getElementById('heart').style.fill = ''
+
+function addFavorite(){
+
+  if(teste = 'white'){
+    document.getElementById('heart').style.fill = '#E63946'
+  } 
+}
+
+function removeFavorite(){
+
+  if(teste = '#E63946'){
+    document.getElementById('heart').style.fill = 'white'
+  } 
+}
+
+function favorite(){
+  teste != '#E63946' ? removeFavorite() : addFavorite() 
+}
