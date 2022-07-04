d08b69264794fd9ef7055872b66a51022388d30e
Souza <joao.souza@capgemini.com>
2022-07-04 09:07:45 -0300

    Adicionando menu mobile resposivo

--- public/src/js/custom/menu.js
+++ public/src/js/custom/menu.js
@@ -1,1 +1,9 @@
 /*----- Arquivo referente a resposividade e animação do component de menu e sidebar do site -----*/
+
+function handleCloseMenu() {
+  document.getElementById("menu").style.transform = "translateY(-100%)";
+}
+
+function handleOpenMenu() {
+  document.getElementById("menu").style.transform = "translateY(0)";
+}
