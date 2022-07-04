c5013e365541c99c90c3e62d700ef6a08988d12b
Souza <joao.souza@capgemini.com>
2022-07-04 15:50:14 -0300

    inserindo tela de produto em foco

---
+++ public/src/js/custom/product-focus.js
@@ -0,0 +1,36 @@
+/* função carousel de imagem */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+/* função adicionar as favoritos*/
+let teste = (document.getElementById("heart").style.fill = "");
+
+function addFavorite() {
+  if ((teste = "white")) {
+    document.getElementById("heart").style.fill = "#E63946";
+  }
+}
+
+function removeFavorite() {
+  if ((teste = "#E63946")) {
+    document.getElementById("heart").style.fill = "white";
+  }
+}
+
+function favorite() {
+  teste != "#E63946" ? removeFavorite() : addFavorite();
+}
